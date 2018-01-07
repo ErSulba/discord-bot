40b9c46a29d8534c80a154c52eb44b4638a6ac23
Alejandro Sulbaran <sulbaran.kun@gmail.com>
2018-01-07 01:01:52 -0400

    testing some jokes

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ bot.on('ready', () => {
 bot.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
-  	}
+    }
+    //Here im trying to joke
+    if (message.content === 'tu eres marisco') {
+      message.reply('mas marisco sereis vos')
+    }
 });
 
 // THIS  MUST  BE  THIS  WAY
